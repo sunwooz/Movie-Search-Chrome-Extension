b9e6763714b7d6ac08ea5dc65b29d40a00108060
sunwooz <yangsunwoo@gmail.com>
2016-02-15 14:40:17 -0500

    version one of movie search chrome extension

---
+++ popup.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $("#search").click(function(e) {
+    searchMovie();
+  });
+
+  $(document).keypress(function(e) {
+    if(e.which == 13) {
+      searchMovie();
+    }
+  });
+});
+
+function searchMovie() {
+  url = "http://www.omdbapi.com/?t=";
+  input = $("#input").val();
+  input = input.replace(/\s/g, "+");
+  url = url + input + "&y=&plot=short&r=json&tomatoes=true"
+
+  $.ajax({
+    dataType: "json",
+    url: url,
+  }).done(function(data) {
+    console.log(data);
+    $('#poster').attr('src', data["Poster"]);
+    $('#title').text(data["Title"]);
+    $('#imdbRating').text(data["imdbRating"]);
+    $('#tomatoMeter').text(data["tomatoMeter"]);
+    $('#released').text(data["Released"]);
+    $('#plot').text(data["Plot"]);
+    $('#dvd').text(data["Dvd"]);
+    $('#genre').text(data["Genre"]);
+  });
+}
+
+
+// var url = "http://www.omdbapi.com/?t=man+of+steel&y=&plot=short&r=json"
+
